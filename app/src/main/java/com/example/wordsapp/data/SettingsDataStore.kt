b3cd771e371c68c30de943ee57eddf6e09226fcb
iAhmed3v <devahmdali@gmail.com>
2022-10-26 20:08:15 +0300

    Implement SettingsDataStore.kt

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -1,10 +1,15 @@
 package com.example.wordsapp.data
 
 import android.content.Context
-import android.preference.PreferenceDataStore
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 
@@ -14,4 +19,31 @@ private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
     name = LAYOUT_PREFERENCES_NAME
 )
 
-class SettingsDataStore(context: Context) {}
+class SettingsDataStore(context: Context) {
+
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+
+    suspend fun saveLayoutToPreferencesStore(
+        isLinearLayoutManager: Boolean
+        , context: Context){
+
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
+
+    val preferenceFlow: kotlinx.coroutines.flow.Flow<Boolean> = context.dataStore.data
+        .catch {
+            if (it is IOException){
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preferences ->
+            // On the first run of the app, we will use LinearLayoutManager by default
+            preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
+        }
+
+}
